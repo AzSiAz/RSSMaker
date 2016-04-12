88c02dc02da5c99fa52d5fa201765626394ef6a3
AzSiAz <slider.perso@live.fr>
2016-04-12 17:39:09 +0200

    update for nanodesu

--- app.js
+++ app.js
@@ -4,44 +4,56 @@ var jsdom = require('jsdom');
 var Feed = require('feed');
 var cloudscraper = require('cloudscraper');
 
-var feed = new Feed({
-  title: 'Baka-Tsuki NovelUpdates',
-  description: 'NovelUpdates Baka-Tsuki group update',
-  link: 'http://www.novelupdates.com/group/baka-tsuki/',
-  image: 'http://www.novelupdates.com/wp-content/uploads/2015/10/ndfavicon.ico',
-  copyright: '',
-  updated: new Date(),
-  author: {
-    name: 'AzSiAz',
-    email: 'contact@azsiaz.tech',
-    link: 'https://azsiaz.tech'
-  }
-});
-var last = {};
-
 app.get('/rss/novelupdates/baka-tsuki', function (req, res) {
+  var feed = new Feed({
+    title: 'Baka-Tsuki NovelUpdates',
+    description: 'NovelUpdates Baka-Tsuki group update',
+    link: 'http://www.novelupdates.com/group/baka-tsuki/',
+    image: 'http://www.novelupdates.com/wp-content/uploads/2015/10/ndfavicon.ico',
+    copyright: '',
+    updated: new Date(),
+    author: {
+      name: 'AzSiAz',
+      email: 'contact@azsiaz.tech',
+      link: 'https://azsiaz.tech'
+    }
+  });
   res.set('Content-Type', 'application/rss+xml');
-  parseData('http://www.novelupdates.com/group/baka-tsuki/', function (data) {
+  parseData('http://www.novelupdates.com/group/baka-tsuki/', feed, function (data) {
     res.send(feed.render('rss-2.0'));
   })
 });
 
 app.get('/rss/novelupdates/nanodesu', function (req, res) {
+  var feed = new Feed({
+    title: 'NanoDesu NovelUpdates',
+    description: 'NovelUpdates NanoDesu group update',
+    link: 'http://www.novelupdates.com/group/nanodesu/',
+    image: 'http://www.novelupdates.com/wp-content/uploads/2015/10/ndfavicon.ico',
+    copyright: '',
+    updated: new Date(),
+    author: {
+      name: 'AzSiAz',
+      email: 'contact@azsiaz.tech',
+      link: 'https://azsiaz.tech'
+    }
+  });
+
   res.set('Content-Type', 'application/rss+xml');
-  parseData('http://www.novelupdates.com/group/nanodesu/', function (data) {
+  parseData('http://www.novelupdates.com/group/nanodesu/', feed, function (data) {
     res.send(feed.render('rss-2.0'));
   })
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello');
-})
+  res.send('RSS Maker');
+});
 
 function correctData(data) {
   
 }
 
-function parseData(url, callback) {
+function parseData(url, feed, callback) {
   cloudscraper.get(url, function(error, response, body) {
   if (error) {
     console.log('Error occurred');
@@ -66,7 +78,7 @@ function parseData(url, callback) {
   }
 });
 }
-// moment(this.cells[0].innerHTML, "MM-DD-YYYY").toDate()
+
 app.listen(4000, function () {
-  console.log('Example app listening on port 4000!');
+  console.log('RSS Maker listening on port 4000!');
 });
