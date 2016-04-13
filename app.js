14db927d0a242678c892411e65c81c07e55729a8
AzSiAz <slider.perso@live.fr>
2016-04-13 19:58:18 +0200

    remove now useless comment

--- app.js
+++ app.js
@@ -9,7 +9,3 @@ app.use('/', require('./server/routes'));
 
 
 var server = http.createServer(app).listen(4000, '127.0.0.1');
-
-// app.listen(4000, function () {
-//   console.log('RSS Maker listening on port 4000!');
-// });
