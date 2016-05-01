0c96eb3b2817603cde2c7bf5e0da1e3f5b28d7ca
AzSiAz <slider.perso@live.fr>
2016-05-01 16:04:03 +0200

    fix missing ;

--- server/utils/parsedata.js
+++ server/utils/parsedata.js
@@ -5,29 +5,23 @@ var madokamidate = require('../utils/madokamidate');
 
 var parseData = {
   novelupdates: function novelupdates(url, feed, callback) {
-    // cloudscraper.get(url, function(error, response, body) {
-    //   if (error) {
-    //     console.log('Error occurred');
-    //   } else {
-        jsdom.env(
-          url,
-          ['http://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js'],
-          function (errors, window) {
-            var $ = window.$;
-            $('#myTable > tbody > tr').each(function (index) {
-              var object = {
-                date: moment.utc(this.cells[0].innerHTML, "MM/DD/YY").toDate(),
-                title: $(this).find('a').first().text() + ' ' + $(this).find('a').last().text(),
-                description: '',
-                link: $(this).find('a').last().attr('href')
-              };
-              feed.addItem(object);
-            });
-            callback();
-          }
-        );
-      // }
-    // });
+    jsdom.env(
+      url,
+      ['http://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js'],
+      function (errors, window) {
+        var $ = window.$;
+        $('#myTable > tbody > tr').each(function (index) {
+          var object = {
+            date: moment.utc(this.cells[0].innerHTML, "MM/DD/YY").toDate(),
+            title: $(this).find('a').first().text() + ' ' + $(this).find('a').last().text(),
+            description: '',
+            link: $(this).find('a').last().attr('href')
+          };
+          feed.addItem(object);
+        });
+        callback();
+      }
+    );
   },
   madokami: function madokami(url, feed, callback) {
     jsdom.env(
@@ -38,19 +32,19 @@ var parseData = {
         $('#index-table > tbody > tr').each(function (index) {
           var title = $(this).find('a').first().text();
           title.endsWith('/') ? title = title.slice(0, title.length - 1) : title;
-          var mdate = madokamidate(this.cells[2].innerHTML.trim())
+          var mdate = madokamidate(this.cells[2].innerHTML.trim());
           var object = {
             date: mdate,
             title: title + ' updated - ' + mdate ,
             description: '',
             link: 'https://manga.madokami.com' + $(this).find('a').first().attr('href')
-          }
+          };
           feed.addItem(object);
-        })
+        });
         callback();
       }
     );
   }
-}
+};
 
 module.exports = parseData;
