7682035991339093f9b836eed892624e96924b67
AzSiAz <slider.perso@live.fr>
2016-06-04 00:05:00 +0200

    test for dokku

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@
 
 var app = require('../');
 
-app.listen(4000, '0.0.0.0', function (err) {
+app.listen(5000, '0.0.0.0', function (err) {
   if (err) throw err;
-  console.log('Server listening on :4000');
+  console.log('Server listening on :5000');
 });
